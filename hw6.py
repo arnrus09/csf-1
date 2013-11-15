f0962e6a857143970d2f04a9e6b7fe4f09f6e0bd
lauton04 <tony.lauricella@gmail.com>
2013-11-15 11:50:08 -0800

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -7,7 +7,7 @@
 # You may do your work by editing this file, or by typing code at the
 # command line and copying it into the appropriate part of this file when
 # you are done.  When you are done, running this file should compute and
-# print the answers to all the problems.
+# print the answers to all the problems. 
 
 
 ###
