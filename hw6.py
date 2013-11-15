cc169a3a2fbde7ce31a758f7bf0a16a6581260ef
lauton04 <tony.lauricella@gmail.com>
2013-11-15 11:33:20 -0800

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Keir Allison-Bourne, Tony Lauricella
+# Evergreen Login: allkei20, lauton04
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 6
@@ -17,8 +17,11 @@
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 3 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+dict1 = {'Sunday' : 1, 'Monday' : 2, 'Tuesday' : 3, 'Wednesday' : 4, 'Thursday' : 5, 'Friday' : 6, 'Saturday' : 7}
 
+dict2 = {1 : 'January', 2 : 'February', 3 : 'March', 4 : 'April', 5 : 'May', 6 : 'June', 7 : 'July', 8 : 'August', 9 : 'September', 10 : 'October', 11 : 'November', 12 : 'December'}
+
+print dict1, dict2
 
 ###
 ### Problem 4
@@ -27,7 +30,12 @@ print "Problem 3 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 4 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+list = [dict1, dict2]
+
+print list
+ 
+#both elements are dictionaries
+#the elements of dict1 are ints and the elements of dict2 are strings ( the elements are the values not the keys )
 
 
 ###
@@ -37,7 +45,14 @@ print "Problem 4 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 5 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+
+listx = [0,0,0,0,0,0,0,0]
+for i in dict1:
+    y = dict1[i]
+    listx[y] = dict2[y]
+    
+listx.remove(0)    
+print listx
 
 ###
 ### Problem 6
@@ -46,7 +61,10 @@ print "Problem 5 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 6 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+#  lines 31 through 32 only pass in a list with one element. lines 34 to 36 pass in a list with two elements. both return dictionaries as their output.
+
+assert state_edges(rows3) == {'WA': 0.9, 'CA': -1.1, 'OR' : 0.2}
+
 
 ###
 ### Problem 7
@@ -55,7 +73,13 @@ print "Problem 6 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 7 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+def month_getter(dict1, dict2):
+    listx = [0,0,0,0,0,0,0,0]
+    for i in dict1:
+        y = dict1[i]
+        listx[y] = dict2[y]
+    listx.remove(0)    
+    return listx
 
 ###
 ### Collaboration
